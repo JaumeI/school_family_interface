6c6e7d57d49eb65a360767655e1e27637933f383
JaumeI <jaumei@gmail.com>
2022-04-27 19:03:21 +0200

    Saving users

--- app/Http/Controllers/Users/UserStoreController.php
+++ app/Http/Controllers/Users/UserStoreController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use function abort_unless;
 
 class UserStoreController extends Controller
@@ -12,8 +16,71 @@ class UserStoreController extends Controller
     public function __invoke(Request $request)
     {
         abort_unless($request->user()->hasPermissionTo('manage_users'), 403, 'You cannot perform this action');
+        $user = User::whereEmail($request->email)->first();
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            //'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        if($user == null)
+        {
+            $user = User::create([
+                'name' =>  $request->name,
+                'email' => $request->email,
+                'email_verified_at' => now(),
+                'password' => Hash::make($request->password),
+                'remember_token' => Str::random(10),
+                'active' => !($request->active == null),
+            ]);
+        }
+        else
+        {
+            $user->name = $request->name;
+            $user->email = $request->email;
+            if(strlen($user->password)>0)
+            {
+                $user->password = Hash::make($request->password);
+            }
+            $user->remember_token =  Str::random(10);
+            $user->active = !($request->active == null);
+            $user->permissions()->detach();
+        }
 
-        //return view('users.index')->with('users', User::orderBy('name')->get());
+        $permissions = array();
+        if($request->permissions == 'admin')
+        {
+            $permissions[] = Permission::where('name', 'manage_users')->first()->id;
+            $permissions[] = Permission::where('name', 'manage_students')->first()->id;
+            $permissions[] = Permission::where('name', 'manage_groups')->first()->id;
+            $permissions[] = Permission::where('name', 'manage_permissions')->first()->id;
+        }
+        else if($request->permissions == 'tutor')
+        {
+            $permissions[] = Permission::where('name', 'see_images')->first()->id;
+            $permissions[] = Permission::where('name', 'start_thread')->first()->id;
+            $permissions[] = Permission::where('name', 'messages')->first()->id;
+            $permissions[] = Permission::where('name', 'manage_tags')->first()->id;
+            $permissions[] = Permission::where('name', 'upload_images')->first()->id;
+
+        }
+        else if ($request->permissions == 'family')
+        {
+            $permissions[] = Permission::where('name', 'see_images')->first()->id;
+            $permissions[] = Permission::where('name', 'start_thread')->first()->id;
+            $permissions[] = Permission::where('name', 'messages')->first()->id;
+        }
+
+
+        $user->permissions()->attach($permissions);
+
+        if(!($request->sendcredentials==null))
+        {
+            //TODO: send credentials to the mail
+        }
+
+        $user->save();
+
+        return view('users.index')->with('users', User::orderBy('name')->get());
     }
 
 }
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,43 +4,78 @@
             <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-100">
 
-                    <form method="POST" action={{ route($user->id == null? 'users.create':'users.update', ['id' => $user->id]) }}>
+                    <form method="POST" action={{ route('users.store', ['id' => $user->id]) }}>
                         @csrf
                         <div class="mb-4">
-                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nom</label>
-                            <input id="name" type="text" value="{{ $user->name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Correu electrònic</label>
+                            <input id="email" name="email" type="email"
+                                   @if(strlen($user->email)>0)
+                                   disabled
+                                   @endif
+                                   value="{{ $user->email }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
-
                         <div class="mb-4">
-                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email address</label>
-                            <input id="email" type="email" value="{{ $user->email }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nom</label>
+                            <input id="name" name="name" type="text" value="{{ $user->name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
                         <div class="mb-4">
                             <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Contrasenya</label>
-                            <input id="password" type="password" value="{{ $user->password }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <input id="password" name="password" type="password" value="" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
                         <div class="mb-4">
-                            <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="radioPermissions" id="radioPermissionFamily">
-                                <label class="form-check-label inline-block text-gray-800" for="flexRadioPermissionFamily">Família</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="radioPermissions" id="radioPermissionTutor">
-                                <label class="form-check-label inline-block text-gray-800" for="flexRadioPermissionTutor">Tutor</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="radioPermissions" id="radioPermissionAdmin">
-                                <label class="form-check-label inline-block text-gray-800" for="flexRadioPermissionAdmin">Admin</label>
-                            </div>
-
-
+                            <input type="checkbox" name="manage_users" value="manage_users" id="manage_users" {{ $user->hasPermissionTo('manage_users')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Gestionar Usuaris</label>
                         </div>
                         <div class="mb-4">
-                            <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="active" {{ $user->active? 'checked' : '' }}>
+                            <input type="checkbox" name="manage_students" value="manage_students" id="manage_students" {{ $user->hasPermissionTo('manage_students')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Gestionar Alumnes</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="manage_groups" value="manage_groups" id="manage_groups" {{ $user->hasPermissionTo('manage_groups')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Gestionar Grups</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="manage_permissions" value="manage_permissions" id="manage_permissions" {{ $user->hasPermissionTo('manage_permissions')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Gestionar Permisos</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="manage_tags" value="manage_tags" id="manage_tags" {{ $user->hasPermissionTo('manage_tags')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Gestionar Etiquetes</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="upload_images" value="upload_images" id="upload_images" {{ $user->hasPermissionTo('upload_images')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Pujar Imatges</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="see_images" value="see_images" id="see_images" {{ $user->hasPermissionTo('see_images')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Veure Imatges</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="start_thread" value="start_thread" id="start_thread" {{ $user->hasPermissionTo('start_thread')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Iniciar converses</label>
+                        </div>
+                        <div class="mb-4">
+                            <input type="checkbox" name="messages" value="messages" id="messages" {{ $user->hasPermissionTo('messages')? 'checked' : '' }}
+                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" >
+                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Enviar Missatges</label>
+                        </div>
+
+
+
+                        <div class="mb-4">
+                            <input name="active" class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="active" id="active" {{ $user->active? 'checked' : '' }}>
                             <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Actiu</label>
                         </div>
                         <div class="mb-4">
-                            <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="sendCredentials" }}>
+                            <input name="sendcredentials" class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600  focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="sendcredentials" id="sendcredentials" }}>
                             <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">Enviar credencials</label>
                         </div>
                         <div class="mb-4">
